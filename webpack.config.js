d33f6aba38336969332239e19d4911e514e0a128
ex-oblivione <keo72792@gmail.com>
2020-08-26 22:27:11 +0300

    Installed cross-env, webpack-dev-server. Created header, src/assets, pages/. webpack.config.js scripts in package.json  was adjusted.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const isDev = process.env.NODE_ENV === 'development'
+console.log("isDev: ", isDev)
+console.log('NODE_ENV', process.env.NODE_ENV)
 // Константа path встроена в node(?!).
 // const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 // MiniCssExtractPlugin нужен только для сборки producrion.
@@ -11,6 +14,12 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
   },
 
   plugins: [
@@ -52,9 +61,17 @@ module.exports = {
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          'file-loader',
-        ],
+        loader: 'file-loader',
+        options: {
+          name: 'assets/fonts/[name].[ext]',
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'assets/images/[name].[ext]',
+        },
       },
 
     ],
