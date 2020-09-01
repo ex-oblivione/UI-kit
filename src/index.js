75caa7faa6e6f7fdc8665a9c4a516157f573a83c
ex-oblivione <keo72792@gmail.com>
2020-09-01 17:23:11 +0300

    Checkbox added. New image Box. New element Masked text field. jQuery added.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import './style.scss';
-import './assets/images/Logo.jpg';
+import './style.scss'
+import './assets/images/Logo.jpg'
+import './assets/images/Box.png'
+import './blocks/form-elements/masked-text-field'
+
 
 // Осталось от тестовой версии для настройки вебпака
 // let sum = function (a, b) {
