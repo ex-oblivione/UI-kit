d6817adf8ba67d262ba43c92b8ffb530bddfbd74
ex-oblivione <keo72792@gmail.com>
2020-10-15 18:40:55 +0300

    Adjust dropdown (jQuery counter)

--- src/blocks/form-elements/dropdown/dropdown__content-counter.js
+++ src/blocks/form-elements/dropdown/dropdown__content-counter.js
@@ -1,15 +1,58 @@
-let count = 0;
-let btnPlus = document.querySelector('.dropdown__content-counter-btn_plus');
-let btnMinus = document.querySelector('.dropdown__content-counter-btn');
+// let count = 0;
+// let btnPlus = document.querySelector('.dropdown__content-counter-btn_plus');
+// let btnMinus = document.querySelector('.dropdown__content-counter-btn');
 
-let counterValue = document.querySelector('.dropdown__content-counter-value');
+// let counterValue = document.querySelector('.dropdown__content-counter-value');
 
-btnPlus.onclick = function () {
-  count++;
-  counterValue.innerHTML = count;
-};
+// btnPlus.onclick = function () {
+//   count++;
+//   counterValue.innerHTML = count;
+// };
 
-btnMinus.onclick = function () {
-  if (count > 0) count--;
-  counterValue.innerHTML = count;
-};
+// btnMinus.onclick = function () {
+//   if (count > 0) count--;
+//   counterValue.innerHTML = count;
+// };
+
+let dropdown = $('.dropdown');
+
+
+$.each(dropdown, function (index) {
+  // console.log('1');
+  let item = $('.dropdown__content-item'),
+    btnClear = $('.dropdown__bottom-item-clear'),
+    dropdownCount = 0;
+
+  btnClear.on('click', function () {
+    dropdownCount = 0
+    $.each(item, (i, e) => {
+      $(e).find('.dropdown__content-counter-value').text(0)
+      // placeholder.text('Сколько гостей')
+    })
+  })
+
+  $.each(item, function (index, elem) {
+    // console.log('2');
+    let itemCount = $(elem).find('.dropdown__content-counter-value'),
+      itemCountValue = +$(elem).find('.dropdown__content-counter-value').text(),
+      btnPlus = $(elem).find('.dropdown__content-counter-btn_plus'),
+      btnMinus = $(elem).find('.dropdown__content-counter-btn_minus');
+
+    // console.log(dropdownCount);
+
+    btnPlus.on('click', function () {
+      itemCountValue = +$(elem).find('.dropdown__content-counter-value').text()
+      if (dropdownCount < 21) dropdownCount++ && itemCount.text(++itemCountValue)
+
+      console.log(dropdownCount);
+    })
+
+    btnMinus.on('click', function () {
+      itemCountValue = +$(elem).find('.dropdown__content-counter-value').text()
+      if (itemCountValue >= 1) {
+        dropdownCount--
+        itemCount.text(--itemCountValue)
+      }
+    })
+  })
+})
