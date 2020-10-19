4e0f27631d725cef34d522a4af317e459bc6c974
ex-oblivione <keo72792@gmail.com>
2020-10-19 12:38:00 +0300

    Adjust dropdown

--- src/blocks/form-elements/dropdown/dropdown__content-counter.js
+++ src/blocks/form-elements/dropdown/dropdown__content-counter.js
@@ -1,58 +1,67 @@
-// let count = 0;
-// let btnPlus = document.querySelector('.dropdown__content-counter-btn_plus');
-// let btnMinus = document.querySelector('.dropdown__content-counter-btn');
-
-// let counterValue = document.querySelector('.dropdown__content-counter-value');
-
-// btnPlus.onclick = function () {
-//   count++;
-//   counterValue.innerHTML = count;
-// };
-
-// btnMinus.onclick = function () {
-//   if (count > 0) count--;
-//   counterValue.innerHTML = count;
-// };
-
 let dropdown = $('.dropdown');
+let placeholder = $('.dropdown__placeholder').text('Сколько гостей');
+
+console.log(placeholder.text());
 
 
 $.each(dropdown, function (index) {
-  // console.log('1');
+
   let item = $('.dropdown__content-item'),
     btnClear = $('.dropdown__bottom-item-clear'),
+    btnApply = $('.dropdown__bottom-item-apply'),
     dropdownCount = 0;
+
+  btnApply.on('click', function () {
+    $('.dropdown__btn,.dropdown__content').toggleClass('active-dropdown');
+  })
 
   btnClear.on('click', function () {
     dropdownCount = 0
     $.each(item, (i, e) => {
       $(e).find('.dropdown__content-counter-value').text(0)
-      // placeholder.text('Сколько гостей')
+      placeholder.text('Сколько гостей')
     })
   })
 
+
   $.each(item, function (index, elem) {
-    // console.log('2');
     let itemCount = $(elem).find('.dropdown__content-counter-value'),
       itemCountValue = +$(elem).find('.dropdown__content-counter-value').text(),
       btnPlus = $(elem).find('.dropdown__content-counter-btn_plus'),
       btnMinus = $(elem).find('.dropdown__content-counter-btn_minus');
 
-    // console.log(dropdownCount);
 
     btnPlus.on('click', function () {
-      itemCountValue = +$(elem).find('.dropdown__content-counter-value').text()
-      if (dropdownCount < 21) dropdownCount++ && itemCount.text(++itemCountValue)
+      itemCountValue = +$(elem).find('.dropdown__content-counter-value').text();
 
-      console.log(dropdownCount);
-    })
+      if (dropdownCount < 20) {
+        ++dropdownCount;
+        itemCount.text(++itemCountValue);
+      }
+      if (dropdownCount === 1) {
+        placeholder.text(dropdownCount + ' гость');
+      } else if (dropdownCount > 1 && dropdownCount < 5) {
+        placeholder.text(dropdownCount + ' гостя');
+      } else {
+        placeholder.text(dropdownCount + ' гостей');
+      }
+    });
 
     btnMinus.on('click', function () {
-      itemCountValue = +$(elem).find('.dropdown__content-counter-value').text()
+      itemCountValue = +$(elem).find('.dropdown__content-counter-value').text();
       if (itemCountValue >= 1) {
-        dropdownCount--
-        itemCount.text(--itemCountValue)
+        --dropdownCount;
+        itemCount.text(--itemCountValue);
       }
-    })
-  })
-})
+      if (dropdownCount === 0) {
+        placeholder.text('Сколько гостей');
+      } else if (dropdownCount === 1) {
+        placeholder.text(dropdownCount + ' гость');
+      } else if (dropdownCount > 1 && dropdownCount < 5) {
+        placeholder.text(dropdownCount + ' гостя');
+      } else {
+        placeholder.text(dropdownCount + ' гостей');
+      }
+    });
+  });
+});
