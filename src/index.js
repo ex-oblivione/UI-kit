8c0229eac9f5e4df0af2bed0a9035a889614d752
ex-oblivione <keo72792@gmail.com>
2020-10-20 08:19:09 +0300

    Improve transition for all elements

--- src/index.js
+++ src/index.js
@@ -4,11 +4,3 @@ import './assets/images/Box.png'
 import './blocks/form-elements/masked-text-field/masked-text-field'
 import './blocks/form-elements/dropdown/dropdown'
 import './blocks/form-elements/dropdown/dropdown__content-counter'
-
-
-// Осталось от тестовой версии для настройки вебпака
-// let sum = function (a, b) {
-//   return a + b;
-// };
-
-// console.log(sum(6, 3));
