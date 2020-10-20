5445fd98a12c78b91e4ea489f1e8278a42c04ce5
ex-oblivione <keo72792@gmail.com>
2020-10-20 18:06:16 +0300

    fix transition

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import './style.scss'
-import './assets/images/Logo.jpg'
 import './assets/images/Box.png'
 import './blocks/form-elements/masked-text-field/masked-text-field'
 import './blocks/form-elements/dropdown/dropdown'
 import './blocks/form-elements/dropdown/dropdown__content-counter'
+
+// Отключение transition при загрузке страницы.
+$("window").load(function () {
+  $("body").removeAttr("id");
+});
